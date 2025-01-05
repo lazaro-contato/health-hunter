32a0616d2c95435b16e2e01adacbfdb7f1e2a448
José Lázaro <contato.jlazaro@gmail.com>
2025-01-05 19:12:53 -0300

    feat: edit main tabs

--- src/app/profile-tab/profile-tab.module.ts
+++ src/app/profile-tab/profile-tab.module.ts
@@ -1,6 +1,6 @@
 import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ProfileTabPage } from './profile-tab.page';
 
@@ -11,7 +11,8 @@ import { ProfilePageRoutingModule } from './profile-tab-routing.module';
     IonicModule,
     CommonModule,
     FormsModule,
-    ProfilePageRoutingModule
+    ProfilePageRoutingModule,
+    NgOptimizedImage
   ],
   declarations: [ProfileTabPage]
 })
