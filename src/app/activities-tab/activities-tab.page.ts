f01bc55e2a0d2ce9ee07de303a42c2f7df2ecab6
José Lázaro <contato.jlazaro@gmail.com>
2025-01-06 12:11:03 -0300

    feat: change todo modal

--- src/app/activities-tab/activities-tab.page.ts
+++ src/app/activities-tab/activities-tab.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {IonModal} from "@ionic/angular";
+import { OverlayEventDetail } from '@ionic/core/components';
 
 @Component({
   selector: 'app-activities',
@@ -7,7 +9,31 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class ActivitiesTabPage {
+  tasks = ['Running', 'Breakfast', 'Vitamin']
 
   constructor() {}
+
+  @ViewChild(IonModal) modal!: IonModal;
+
+  message = 'This modal example uses triggers to automatically open a modal when the button is clicked.';
+  name!: string;
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modal.dismiss(this.name, 'confirm');
+  }
+
+  onWillDismiss(event: CustomEvent<OverlayEventDetail>) {
+    if (event.detail.role === 'confirm') {
+      this.message = `Hello, ${event.detail.data}!`;
+    }
+  }
+
+  customCounterFormatter(inputLength: number, maxLength: number) {
+    return `${maxLength - inputLength} characters remaining`;
+  }
 
 }
--- src/app/components/to-do-item/to-do-item.component.ts
+++ src/app/components/to-do-item/to-do-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-to-do-item',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./to-do-item.component.scss'],
 })
 export class ToDoItemComponent  implements OnInit {
+  @Input() todoTitle!: string;
 
   constructor() { }
 
